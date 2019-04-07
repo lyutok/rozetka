a9119a754e58772148a25413278bff69d97d7bf1
tokarlyudmila <tokarlyudmila@gmail.com>
2019-04-07 22:20:03 +0300

    Add project files.

---
+++ RozetkaUa/RozetkaObjects.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace RozetkaUa
+{
+    class RozetkaObjects
+    {
+        public IWebDriver driver;
+
+
+        public RozetkaObjects(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+            
+        }
+
+        [FindsBy(How = How.XPath, Using = "/html/body/app-root/div/div[1]/div[1]/header/div/div[2]/div[2]/fat-menu/div/ul/li[1]/a")]
+        public IWebElement laptopsAndComputers;
+
+        [FindsBy(How = How.XPath, Using = "/html/body/app-root/div/div[1]/div[1]/header/div/div[2]/div[2]/fat-menu/div/ul/li[1]/div/div[2]/div[1]/div[1]/ul[1]/li/ul/li[1]/a")]
+        public IWebElement asus;
+
+    }
+}
---
+++ RozetkaUa/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+
+namespace RozetkaUa
+{
+    public class UnitTest1
+    {
+        IWebDriver driver;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://rozetka.com.ua");
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void Test()
+        {
+            RozetkaObjects rozetkaObjects = new RozetkaObjects(driver);
+            var laptopsAndComputers = rozetkaObjects.laptopsAndComputers;
+            var asus = rozetkaObjects.asus;
+
+
+            // IWebElement element = null;
+            //  IWebElement target = null;
+            Actions actions = new Actions(driver);
+            actions.Build();
+            IAction action = actions.Build();
+            actions.Click(laptopsAndComputers);
+                // .MoveToElement(asus).Click().Release();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+        }
+    }
+}
